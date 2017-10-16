bf842dd37115b2faba24a984479771ce87cce6b3
jambion <ambion_j@yahoo.com>
2017-10-15 19:38:18 -0700

    Added Edit And Update Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,22 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+      
+  end
+  
+  
   def create
     @article = Article.new(article_params)
     
